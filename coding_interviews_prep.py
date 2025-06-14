af8709645e0ab4d0f507e8e62c7bbe49bacde94f
TanmaySamdani19 <tanmaysamdani12345@gmail.com>
2025-06-14 22:59:45 +0100

    prep

--- coding_interviews_prep.py
+++ coding_interviews_prep.py
@@ -205,3 +205,175 @@ print(ord("b"))
 # COmbine a list of strings (with an empty stirng delimitor)
 strings = ["ab", "cd", "ef"]
 print("".join(strings))
+
+# Queues double ended queue
+from collections import deque
+
+queue = deque()
+queue.append(1)
+queue.append(2)
+print(queue)
+
+# similar to stack in python but benefit is we can pop from left and we can do it in cosntant time unlike stack
+queue.popleft()
+print(queue)
+
+queue.appendleft(1)
+print(queue)
+
+queue.pop()
+print(queue)
+
+
+# HashSet
+mySet = set()
+mySet.add(1)
+mySet.add(2)
+print(mySet)
+print(len(mySet))
+print(1 in mySet)
+mySet.remove(2)
+print(2 in mySet)
+
+# list to set
+print(set([1, 2, 3]))
+
+# Set comprehension
+mySet =  { i for i in range(5)}
+print(mySet)
+
+
+# HashMap (aka dict)
+myMap = {}
+myMap["alice"] = 88
+myMap["bob"] = 77
+print(myMap)
+print(len(myMap))
+
+myMap['alice'] = 80
+print(myMap['alice'])
+
+print('alice' in myMap)
+myMap.pop('alice')
+print('alice' in myMap)
+
+myMap = {'alice' :90, 'bob':70}
+print(myMap)
+
+# Dict comprehension
+myMap = {i : 2*i for i in range(3)}
+print (myMap)
+
+# Looping through maps
+for key in myMap:
+    print(key, myMap[key])
+
+for val in myMap.values():
+    print(val)
+
+for key, val in myMap.items():
+    print(key, val)
+
+
+# Tuples are like arrays but immutable
+tup = {1,2,3}
+print(tup)
+# print(tup[0])
+
+# Can't modify
+
+# Can be used as key for hash map/set
+myMap = { (1,2): 3}
+print(myMap[(1,2)])
+
+
+# mySet = set()
+# mySet.add((1,2))
+# print((1,2) in mySet)
+
+# Lists cant be keys
+# myMap[[3,4]] = 5  -this will not work so we use tuple 
+
+# Heaps
+import heapq
+# under the hood are arrays
+minHeap = []
+heapq.heappush(minHeap,3)
+heapq.heappush(minHeap,2)
+heapq.heappush(minHeap,4)
+
+#  min is always at index 0
+print(minHeap[0])
+
+while len(minHeap):
+    print(heapq.heappop(minHeap))
+
+# No max heaps by default, work around is to use min heap and multiply by -1 when push and pop.
+maxHeap = []
+heapq.heappush(maxHeap, -3)
+heapq.heappush(maxHeap, -2)
+heapq.heappush(maxHeap, -4)
+
+# Max is always at index 0
+print(-1* maxHeap[0])
+
+while len(maxHeap):
+    print(-1*heapq.heappop(maxHeap))
+
+# Build heap from iniial values
+arr = [2,1,8,4,5]
+
+heapq.heapify(arr)
+while arr:
+    print(heapq.heappop(arr))
+
+# Funcitons
+def myFunc(n, m):
+    return n* m
+
+
+# Nested funciton 
+def outer(a,b):
+    c = "c"
+
+    def inner():
+        return a+b+c
+    return inner()
+
+print(outer("a","b"))
+
+# Can modify objects but not reassign
+# unless using nonlocal keyboard
+def double(arr, val):
+    def helper():
+        for i, n  in enumerate(arr):
+            arr[i] *= 2
+
+        # will only modify val in the helper scope
+        # val *= 2
+
+        # this will modify val outside helper scope
+        nonlocal val
+        val *=2
+
+    helper()
+    print(arr, val)
+
+nums = [1,2]
+val = 3
+double(nums,val)  
+
+# Class
+class MyClass:
+    # Constructor
+    def __init__(self, nums):
+        # Create member variables
+        self.nums = nums
+        self.size = len(nums)
+
+    # sef keyword required as param
+    def getLength(self):
+        return self.size
+    
+    def getDoubleLength(self):
+        return 2*self.getLength()
